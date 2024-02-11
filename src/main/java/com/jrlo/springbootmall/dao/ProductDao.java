27f14e923a24522517e0567d1d676116a394465e
LYP0416 <brandon416jr@gmail.com>
2024-02-11 23:13:09 +0800

    商品功能 - 實作商品查詢列表 (基礎實作)

--- src/main/java/com/jrlo/springbootmall/dao/ProductDao.java
+++ src/main/java/com/jrlo/springbootmall/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.jrlo.springbootmall.dao;
 import com.jrlo.springbootmall.dto.ProductRequest;
 import com.jrlo.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/jrlo/springbootmall/service/ProductService.java
+++ src/main/java/com/jrlo/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.jrlo.springbootmall.service;
 import com.jrlo.springbootmall.dto.ProductRequest;
 import com.jrlo.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
