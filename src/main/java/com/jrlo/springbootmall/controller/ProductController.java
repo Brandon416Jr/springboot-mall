22d3e76c2100a95acd8c2835f632f7b03dc5b6ee
LYP0416 <brandon416jr@gmail.com>
2024-02-12 14:37:20 +0800

    商品功能 - 實作查詢商品列表 (分頁 + 總比數)

--- src/main/java/com/jrlo/springbootmall/controller/ProductController.java
+++ src/main/java/com/jrlo/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.jrlo.springbootmall.dao.ProductQueryParams;
 import com.jrlo.springbootmall.dto.ProductRequest;
 import com.jrlo.springbootmall.model.Product;
 import com.jrlo.springbootmall.service.ProductService;
+import com.jrlo.springbootmall.util.Page;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -24,7 +25,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             // 查詢條件filtering
             @RequestParam (required = false) ProductCategory category,
             @RequestParam (required = false) String search,
@@ -45,9 +46,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        // 取得productList
         List<Product> productList = productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        // 取得product總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResult(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
     @GetMapping("/products/{productId}")
